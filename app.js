66dfe95b9e900a253910415625aabc7c7413285a
João Pedro Limão <jplimao12@gmail.com>
2020-05-24 05:49:50 -0300

    Changing variables

--- app.js
+++ app.js
@@ -62,3 +62,7 @@ function recreateWindow() {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+module.exports = {
+  currentWindow: win
+}
